e5892a95935af89b8d2e3a176a29345f69a9029b
calvinreu <calvinreu@localhost.localdomain>
2020-12-27 18:32:40 +0100

    Load Config files

--- src/cmd/config.go
+++ src/cmd/config.go
@@ -1,9 +1,55 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
 //Config constains all the data
 type Config struct {
 	EnergyLossMultiplier, AverageWaterPercentage, MinWaterRequirement, FoodGrowthSpeedPerWaterPercent, WaterPerFood float64
-	MapHeight, MapWidth, Seed, ContinentCount                                                                       uint16
+	MapHeight, MapWidth, Seed, ContinentCount, ReproductionCost                                                     uint16
 }
 
-func LoadConfig
+//TempConfig is the struct to contain json info
+type TempConfig struct {
+	EnergyLossMultiplier, AverageWaterPercentage, MinWaterRequirement, FoodGrowthSpeedPerWaterPercent, WaterPerFood, MapHeight, MapWidth, Seed, ContinentCount, ReproductionCost float64
+}
+
+//Load sets config from file
+func (config *Config) Load(filePath string) {
+	var tempConfig TempConfig
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Print(err)
+	}
+	err = json.Unmarshal(data, &tempConfig)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	config.EnergyLossMultiplier = tempConfig.EnergyLossMultiplier
+	config.AverageWaterPercentage = tempConfig.AverageWaterPercentage
+	config.MinWaterRequirement = tempConfig.MinWaterRequirement
+	config.FoodGrowthSpeedPerWaterPercent = tempConfig.FoodGrowthSpeedPerWaterPercent
+	config.WaterPerFood = tempConfig.WaterPerFood
+	config.MapHeight = (uint16)(tempConfig.MapHeight)
+	config.MapWidth = (uint16)(tempConfig.MapWidth)
+	config.Seed = (uint16)(tempConfig.Seed)
+	config.ContinentCount = (uint16)(tempConfig.ContinentCount)
+	config.ReproductionCost = (uint16)(tempConfig.ReproductionCost)
+
+	fmt.Println("Config:")
+	fmt.Println("EnergyLossMultiplier:", config.EnergyLossMultiplier)
+	fmt.Println("AverageWaterPercentage", config.AverageWaterPercentage)
+	fmt.Println("MinWaterRequirement", config.MinWaterRequirement)
+	fmt.Println("FoodGrowthSpeedPerWaterPercent", config.FoodGrowthSpeedPerWaterPercent)
+	fmt.Println("WaterPerFood", config.WaterPerFood)
+	fmt.Println("MapHeight", config.MapHeight)
+	fmt.Println("MapWidth", config.MapWidth)
+	fmt.Println("Seed", config.Seed)
+	fmt.Println("ContinentCount", config.ContinentCount)
+	fmt.Println("ReproductionCost", config.ReproductionCost)
+
+}
--- src/cmd/evosim.go
+++ src/cmd/evosim.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+var config Config
+
 func main() {
 
+	args := os.Args[1:]
+	if len(args) > 2 {
+		fmt.Println("to many aunch arguments")
+	}
+	if len(args) == 0 {
+		fmt.Println("to few args")
+		return
+	}
+	config.Load(args[0])
 }
