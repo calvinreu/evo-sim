e34e3a4fd984e91539d192bf34fdedccc6e264f0
calvinreu <j.calvin.reu@protonmail>
2020-12-28 17:43:36 +0100

    imported and changed graphics lib

---
+++ src/lib/graphic/example/example.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+
+	graphic ".."
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+var sprite1 uint32
+var sprite2 uint32
+
+var instances1 [10]list.Element
+var instances2 [20]list.Element
+
+func main() {
+	example, err := graphic.New("example", 0, 0, 1920, 1080, sdl.WINDOW_FULLSCREEN, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	sprite1, err = example.AddSprite("./img/wololo.png", sdl.Rect{0, .0, 400, 200})
+	sprite2, err = example.AddSprite("./img/blabla.png", sdl.Rect{0, .0, 200, 400})
+
+	for i := 0; i < len(instances1); i++ {
+		instances1[i].Value = example.Sprites[sprite1].NewInstance((float64)(i*36), &sdl.Point{(int32)(i*192 + 100), (int32)(i*108 + 100)})
+	}
+
+	for i := 0; i < len(instances1); i++ {
+		instances2[i].Value = example.Sprites[sprite2].NewInstance((float64)(i*36), &sdl.Point{(int32)(i * 192), (int32)(i*108 + 100)})
+	}
+	running := true
+	go example.RunOutput(10, &running)
+	sdl.Delay(1000)
+	running = false
+
+	fmt.Print("the result should look very chaotic \n")
+
+	sdl.Quit()
+}
---
+++ src/lib/graphic/sprite.go
@@ -0,0 +1,72 @@
+//Package graphic is using the sdl2 go interface from (c)https://github.com/veandco/go-sdl2/ under the BSD 3 License
+package graphic
+
+import (
+	"container/list"
+
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+//Instance position Angle and the Center of an instance of a sprite
+type Instance struct {
+	DestRect sdl.FRect
+	Angle    float64
+	Center   sdl.FPoint
+}
+
+//Sprite contains the texture a list of instances and a srcRect
+type Sprite struct {
+	texture   *sdl.Texture
+	instances *list.List
+	srcRect   sdl.Rect
+}
+
+//NewSprite creates a sprite based on a renderer, the image path and a src rectAngle
+func NewSprite(renderer *sdl.Renderer, imgPath string, srcRect sdl.Rect) (Sprite, error) {
+	var sprite Sprite
+	var err error
+	sprite.texture, err = img.LoadTexture(renderer, imgPath)
+
+	if err != nil {
+		return sprite, err
+	}
+
+	sprite.srcRect = srcRect
+	sprite.instances = list.New()
+
+	return sprite, err
+}
+
+//NewInstance adds a instance to the sprite and initializes the width and height of the dest rectAngle with the src rectAngle
+func (sprite *Sprite) NewInstance(Angle float64, Center *sdl.FPoint) *Instance {
+	var instance Instance
+
+	instance.DestRect.W = (float32)(sprite.srcRect.W)
+	instance.DestRect.H = (float32)(sprite.srcRect.H)
+	instance.NewPosition(Center)
+	instance.Angle = Angle
+
+	if instance, ok := sprite.instances.PushBack(&instance).Value.(*Instance); ok {
+		return instance
+	}
+
+	println("fatal error &instance in func NewInstace is no *Instance")
+	return nil
+}
+
+//NewPosition sets the position of this instance Center is the Center of the instances new position
+func (instance *Instance) NewPosition(Center *sdl.FPoint) {
+	instance.Center.X = Center.X
+	instance.Center.Y = Center.Y
+	instance.DestRect.X = Center.X - (instance.DestRect.W / 2)
+	instance.DestRect.Y = Center.Y - (instance.DestRect.H / 2)
+}
+
+//ChangePosition moves the instance by x, y
+func (instance *Instance) ChangePosition(x, y float32) {
+	instance.Center.X += x
+	instance.Center.Y += y
+	instance.DestRect.X = instance.Center.X - (instance.DestRect.W / 2)
+	instance.DestRect.Y = instance.Center.Y - (instance.DestRect.H / 2)
+}
