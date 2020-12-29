3da49b35c7476dd870bb9a292935fa5e122b0989
calvinreu <j.calvin.reu@protonmail>
2020-12-29 02:12:47 +0100

    added ui init

--- src/cmd/evosim.go
+++ src/cmd/evosim.go
@@ -5,17 +5,24 @@ import (
 	"os"
 )
 
-var config Config
-
 func main() {
+
+	var config Config
 
 	args := os.Args[1:]
 	if len(args) > 2 {
 		fmt.Println("to many aunch arguments")
 	}
 	if len(args) == 0 {
-		fmt.Println("to few args")
+		fmt.Println("to few arguments")
 		return
 	}
 	config.Load(args[0])
+
+	configureGraphic(&config)
+
+	var endProgramm string
+
+	fmt.Scanln(&endProgramm)
+
 }
