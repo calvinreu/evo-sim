6a6f6cd7dbc93beb66e76ff18b40e42d61a647ee
calvinreu <j.calvin.reu@protonmail>
2020-12-31 20:15:54 +0100

    added options via terminal

--- src/cmd/evosim.go
+++ src/cmd/evosim.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"container/list"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"../lib/config"
@@ -18,17 +20,33 @@ func main() {
 	var window graphic.Graphic
 	var command string
 	var rendererRunning bool = true
-	var creatures *list.List
+	creatures := list.New()
 
 	args := os.Args[1:]
 	if len(args) > 2 {
 		fmt.Println("to many aunch arguments")
 	}
-	if len(args) == 0 {
+	if len(args) < 1 {
 		fmt.Println("to few arguments")
 		return
 	}
-	config.Load(args[0])
+
+	fmt.Println(args[0])
+
+	switch args[0] {
+	case "--help":
+		terminalHelp()
+		return
+	case "--load":
+		loadSaveFile(args[1], &config, &chart, creatures)
+		break
+	case "--new":
+		config.Load(args[1])
+		//add loading and creating of all the other parts
+		break
+	default:
+		fmt.Println("type --help for help")
+	}
 
 	window.Configure(&config, &chart)
 
@@ -50,4 +68,25 @@ func main() {
 
 	sdl.Quit()
 
+}
+
+type Commands struct {
+	Commands []string
+}
+
+func terminalHelp() {
+	//load commands
+	var commands Commands
+	data, err := ioutil.ReadFile("../../resources/cmd args.json")
+	if err != nil {
+		fmt.Print(err)
+	}
+	err = json.Unmarshal(data, &commands)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	for _, i := range commands.Commands {
+		fmt.Println(i)
+	}
 }
