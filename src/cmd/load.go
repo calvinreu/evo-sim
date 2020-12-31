b239acb4d84e169860f14908e85ebba4027aa456
calvinreu <j.calvin.reu@protonmail>
2020-12-31 20:18:10 +0100

    added load function

--- src/cmd/load.go
+++ src/cmd/load.go
@@ -12,18 +12,18 @@ import (
 
 //SaveFile contains json info of the main save file
 type SaveFile struct {
-	configFile, mapFile string
-	creatures           []string
+	ConfigFile, MapFile string
+	Creatures           []string
 }
 
-func loadSaveFile(filename string, config *config.Config, chart *environment.Map, creatures *list.List) map[uint32]*environment.Creature {
+func loadSaveFile(filename string, config *config.Config, chart *environment.Map, creatures *list.List) {
 	//load main save file
 	var tempSaveFile SaveFile
-	creatureIDs := 
+	creatureIDs := make(map[uint32]*environment.Creature)
 
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println("error:", err)
 	}
 	err = json.Unmarshal(data, &tempSaveFile)
 	if err != nil {
@@ -31,21 +31,23 @@ func loadSaveFile(filename string, config *config.Config, chart *environment.Map
 	}
 
 	fmt.Println("Save Files:")
-	fmt.Println("Config file: ", tempSaveFile.configFile)
-	fmt.Println("Map file: ", tempSaveFile.mapFile)
-	fmt.Println("Creature files: ", tempSaveFile.creatures)
+	fmt.Println("Config file: ", tempSaveFile.ConfigFile)
+	fmt.Println("Map file: ", tempSaveFile.MapFile)
+	fmt.Println("Creature files: ", tempSaveFile.Creatures)
+	fmt.Println("----------")
 
-	config.Load(tempSaveFile.configFile)
-	chart.Load(tempSaveFile.mapFile)
+	config.Load(tempSaveFile.ConfigFile)
 
-	for i := 0; i < len(tempSaveFile.creatures); i++ {
+	for i := 0; i < len(tempSaveFile.Creatures); i++ {
 		var tempCreature environment.Creature
-		tempCreature.Load(tempSaveFile.creatures[i])
-		if creature, ok := creatures.PushBack(&tempCreature).Value.(*Instance); ok {
-
+		tempCreature.Load(tempSaveFile.Creatures[i])
+		if creature, ok := creatures.PushBack(&tempCreature).Value.(*environment.Creature); ok {
+			creatureIDs[uint32(i)] = creature
 		} else {
-			fmt.Println("type error creatures.PushBack does not return *Creature")
+			fmt.Println("type error creatures.PushBack does not return *environment.Creature")
 		}
+		fmt.Println("-----------")
 	}
 
+	chart.Load(tempSaveFile.MapFile, creatureIDs)
 }
--- src/lib/environment/map.go
+++ src/lib/environment/map.go
@@ -1,5 +1,22 @@
 package environment
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+//MapSave contains map json data
+type MapSave struct {
+	Fields [][]FieldSave
+}
+
+//FieldSave contains Field json data
+type FieldSave struct {
+	Food, WaterPercentage float64
+	Creatures             []uint32
+}
+
 //Field contains the information of one square of the map
 type Field struct {
 	Creatures             []*Creature
@@ -12,6 +29,52 @@ type Map struct {
 }
 
 //Load a map from file
-func (m *Map) Load(filename string) {
+func (m *Map) Load(filename string, creatureIDs map[uint32]*Creature) {
+	var tempSaveFile MapSave
 
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	err = json.Unmarshal(data, &tempSaveFile)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	if len(tempSaveFile.Fields) == 0 {
+		fmt.Println("Map json file empty")
+		return
+	}
+
+	fmt.Println("Map size x/y: ", len(tempSaveFile.Fields), len(tempSaveFile.Fields[0]))
+
+	fmt.Println("----------")
+
+	for i := 0; i < len(tempSaveFile.Fields); i++ {
+		var colunm []Field
+		m.Fields = append(m.Fields, colunm)
+		for _, j := range tempSaveFile.Fields[i] {
+			tempField := j.CreateField(creatureIDs)
+			m.Fields[i] = append(m.Fields[i], tempField)
+		}
+	}
+}
+
+//create a Field from Field save
+func (fieldSave *FieldSave) CreateField(creatureIDs map[uint32]*Creature) Field {
+	var field Field
+	field.Food = fieldSave.Food
+	field.WaterPercentage = fieldSave.WaterPercentage
+
+	for _, i := range fieldSave.Creatures {
+		field.Creatures = append(field.Creatures, creatureIDs[i])
+	}
+
+	fmt.Println("Field:")
+	fmt.Println("Water percentage: ", field.WaterPercentage)
+	fmt.Println("Food: ", field.Food)
+	fmt.Println("Creatures: ", field.Creatures)
+	fmt.Println("----------")
+
+	return field
 }
