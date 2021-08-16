84f2b3df9552420c3a802595d230b9e7aefff60e
calvinreu <j.calvin.reu@protonmail.com>
2021-08-16 12:41:22 +0200

    clean up code

--- src/lib/graphic/sprite.go
+++ src/lib/graphic/sprite.go
@@ -8,16 +8,6 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-//Instance position Angle and the Center of an instance of a sprite
-type Instance struct {
-	DestRect sdl.FRect
-	Angle    float64
-	Center   sdl.FPoint
-}
-
-//CreatureInstance is type used by renderer for every instance of a creature
-type CreatureInstance *sdl.FPoint
-
 //Sprite contains the texture a list of instances and a srcRect
 type Sprite struct {
 	texture   *sdl.Texture
@@ -56,20 +46,4 @@ func (sprite *Sprite) NewInstance(Angle float64, Center *sdl.FPoint) *Instance {
 
 	println("fatal error &instance in func NewInstace is no *Instance")
 	return nil
-}
-
-//NewPosition sets the position of this instance Center is the Center of the instances new position
-func (instance *Instance) NewPosition(Center *sdl.FPoint) {
-	instance.Center.X = Center.X
-	instance.Center.Y = Center.Y
-	instance.DestRect.X = Center.X - (instance.DestRect.W / 2)
-	instance.DestRect.Y = Center.Y - (instance.DestRect.H / 2)
-}
-
-//ChangePosition moves the instance by x, y
-func (instance *Instance) ChangePosition(x, y float32) {
-	instance.Center.X += x
-	instance.Center.Y += y
-	instance.DestRect.X = instance.Center.X - (instance.DestRect.W / 2)
-	instance.DestRect.Y = instance.Center.Y - (instance.DestRect.H / 2)
 }
